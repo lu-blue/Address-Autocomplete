ea3fc3c60ae4fc6ccd6ff352005c707e82cfd5e0
Lubov <lubov.novozhilova@gmail.com>
2021-12-14 14:18:27 +0100

    Logic for rendering components on page

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import GoogleMap from "./components/GoogleMap";
+import AddressForm from "./components/AddressForm";
+import { useState } from "react";
+import "./css/App.css";
+
+
+
+export default function App() {
+  const [viewMap, setViewMap] = useState(false);
+  const linkName = viewMap
+    ? "Close map search"
+    : "Search your address with Google Maps";
+
+  const extraMap = <GoogleMap />;
+
+  return (
+    <div className="App">
+      <h2>Please, fill in your address</h2>
+      <AddressForm />
+
+      <div className="More">
+        <button
+          className="view-more-button"
+          onClick={() => {
+            setViewMap(!viewMap);
+          }}
+        >
+          {linkName}
+        </button>
+        {viewMap && extraMap}
+      </div>
+    </div>
+  );
+}
